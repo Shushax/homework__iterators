898bf117eb36a9fda887994390d6df904f842bc3
Ksenia Sidorova <nekfi@mail.ru>
2020-10-14 02:02:18 +0300

    Добавлен черновик 1 задания

--- src/js/symbols_and_generations.js
+++ src/js/symbols_and_generations.js
@@ -12,3 +12,37 @@ export default class Team {
         yield this.char3
     }
 }
+
+// черновик первого задания
+
+// export default class Team {
+
+//     constructor(char1, char2, char3) {
+//         this.char1 = char1,
+//         this.char2 = char2,
+//         this.char3 = char3
+//     }
+
+//     [Symbol.iterator]() {
+//         let current = this.char1;
+//         let last = this.char3;
+
+//         return {
+//             next() {
+//                 if (current !== last) {
+//                     return {
+//                         done: false,
+//                         value: ??????
+//                     }
+
+//                 }
+
+//                 return {
+//                     done: true
+//                 }
+//             }
+//         } 
+        
+
+//     }
+// }
